7903f21d4feca7fda018eef027ac5d22fd80fb88
Ben Zeltser <zeltserben@Hotmail.com>
2023-11-14 20:22:39 +0200

    final desings applied. show more button works.

--- script.js
+++ script.js
@@ -11,18 +11,36 @@
 
  async function searchImages() {
     keyword = searchBox.value;
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
 
     const response = await fetch(url);
     const data = await response.json();
 
-    console.log(data);
+    const results = data.results;
+
+    results.map((result) => {
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+
+        imageLink.appendChild(image);
+        searchResult.appendChild(imageLink);
+    })
+
+    ShowMoreBtn.style.display = "block";
  }
     searchForm.addEventListener("submit", (e) =>{
         e.preventDefault();
         page = 1;
         searchImages();
      })
+
+    ShowMoreBtn.addEventListener("click", ()=>{
+        page++;
+        searchImages();
+    })
  
 
  
