f835c307c925b872223efe14a9227e5d6633f462
Ben Zeltser <zeltserben@Hotmail.com>
2023-11-14 17:20:10 +0200

    Added JS file and some css changes

---
+++ script.js
@@ -0,0 +1,6 @@
+ 
+ const searchForm = document.getElementById("serach-form");
+ const searchBox = document.getElementById("search-box");
+ const searchResult = document.getElementById("search-result");
+ const ShowMoreBtn = document.getElementById("show-more-btn");
+ 
