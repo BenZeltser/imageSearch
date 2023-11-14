a7699208cdefd70f9491db824d929f30998d7e58
Ben Zeltser <zeltserben@Hotmail.com>
2023-11-14 19:07:48 +0200

    Major fetching bug fixed

--- script.js
+++ script.js
@@ -11,18 +11,18 @@
 
  async function searchImages() {
     keyword = searchBox.value;
-    const url = 'https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}';
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
 
     const response = await fetch(url);
     const data = await response.json();
 
     console.log(data);
-
-
  }
+    searchForm.addEventListener("submit", (e) =>{
+        e.preventDefault();
+        page = 1;
+        searchImages();
+     })
+ 
 
- searchForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    page = 1;
-    searchImages();
- })
+ 
